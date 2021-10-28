98a03c7cb2baf96ec877940c8d1465d7c638571a
Chaitanya Patel <chaitanya.patel@appnovation.com>
2021-10-28 17:54:23 -0400

    Finished Gatsby Tutorial Part 5

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,19 +1,31 @@
 module.exports = {
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
-    title: "gatsbytutorialappno",
+    title: "gatsby tutorial appno",
   },
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    "gatsby-plugin-mdx",
     "gatsby-transformer-sharp",
     {
       resolve: "gatsby-source-filesystem",
       options: {
-        name: "images",
-        path: "./src/images/",
+        name: "blog",
+        path: `${__dirname}/blog`,
       },
       __key: "images",
     },
   ],
 };
+
+
+
+// {
+//   resolve: `gatsby-source-contentful`,
+//   options: {
+//     spaceId: process.env.CONTENTFUL_SPACE_ID,
+//     accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+//     host: `preview.contentful.com`,
+//   },
+// },
